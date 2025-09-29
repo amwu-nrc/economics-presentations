f9482e376acd6739311de41ab751f9e229b00c58
Hamish Gamble <hamish.gamble@amwu.org.au>
2025-09-29 14:35:34 +1000

    make pdf

--- to_pdf.R
+++ to_pdf.R
@@ -1,2 +1,1 @@
-shell("cd docs && docker run --rm -t -v .:/slides astefanutti/decktape -s 1280x720 generic /slides/delegates-forum.html delegates-forum-slides.pdf")
 shell("cd docs && docker run --rm -t -v .:/slides astefanutti/decktape -s 1280x720 generic /slides/du4-economics-political-economy-hamish-gamble.html du4-slides.pdf")
